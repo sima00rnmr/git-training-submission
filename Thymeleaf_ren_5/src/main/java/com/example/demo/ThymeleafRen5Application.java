26890baa73ae6e926538daf8a4c09d87180de2d9
sima00rnmr <sima92rnmr@gmail.com>
2026-03-02 11:47:14 +0900

    前課題完了＆新規課題ファイル作成まで

---
+++ Thymeleaf_ren_5/src/main/java/com/example/demo/ThymeleafRen5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafRen5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafRen5Application.class, args);
+	}
+
+}
